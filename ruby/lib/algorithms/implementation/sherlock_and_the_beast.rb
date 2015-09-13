b48a8b9edde8a1dda4a4b68f9e13e84e34c85a77
Alabê Duarte <alabeduarte@gmail.com>
2015-09-13 16:49:12 -0300

    Rewrite sherlock algorithm to a new attempt

--- ruby/lib/algorithms/implementation/sherlock_and_the_beast.rb
+++ ruby/lib/algorithms/implementation/sherlock_and_the_beast.rb
@@ -2,52 +2,84 @@ module Hackerhank
   module Algorithms
     module Implementation
       class Sherlock
-        SMALLER_DECENT_VALUE = 3
-        BIGGER_DECENT_VALUE = 5
-
-        def initialize(number_of_digits)
-          @number_of_digits = number_of_digits
-        end
-
-        def decent_number
-          return decent_digit_splitited if (bigger_than_decent_number?)
-
-          result = ''
-          number_of_digits.times { result << decent_digit }
-
-          result
+        def decent_number(number_of_digits)
+          bigger_digit?(number_of_digits) ?
+            RecursiveDecentNumber.new(number_of_digits).to_s :
+            DecentNumber.new(number_of_digits).to_s
         end
 
         private
 
-        def decent_digit
-          case number_of_digits
-          when ->(n) { n % SMALLER_DECENT_VALUE == 0 }
-            BIGGER_DECENT_VALUE.to_s
-          when ->(n) { n % BIGGER_DECENT_VALUE == 0 }
-            SMALLER_DECENT_VALUE.to_s
-          else
-            '-1'
+        def bigger_digit?(number_of_digits)
+          DecentNumber.new(number_of_digits)
+            .neither_divisible_or_smaller?
+        end
+
+        class DecentNumber
+          SMALLER_DECENT_VALUE = 3
+          BIGGER_DECENT_VALUE = 5
+
+          def initialize(number_of_digits)
+            @number_of_digits = number_of_digits
+          end
+
+          def to_s
+            case @number_of_digits
+            when ->(n) { divisible_by_3?(n) }
+              build_with(BIGGER_DECENT_VALUE)
+            when ->(n) { divisible_by_5?(n) }
+              build_with(SMALLER_DECENT_VALUE)
+            else
+              '-1'
+            end
+          end
+
+          def divisible_by_5?(number)
+            number % BIGGER_DECENT_VALUE == 0
+          end
+
+          def divisible_by_3?(number)
+            number % SMALLER_DECENT_VALUE == 0
+          end
+
+          def neither_divisible_or_smaller?
+            !divisible_by_3?(@number_of_digits) &&
+              !divisible_by_5?(@number_of_digits) &&
+              @number_of_digits > SMALLER_DECENT_VALUE &&
+              @number_of_digits > BIGGER_DECENT_VALUE
+          end
+
+          private
+
+          def build_with(decent_value)
+            Array.new(@number_of_digits, decent_value).join
           end
         end
 
-        def bigger_than_decent_number?
-          splitted_factor > BIGGER_DECENT_VALUE
+        class RecursiveDecentNumber < DecentNumber
+          def to_s
+            find_decent_number(@number_of_digits, 0).join
+          end
+
+          def find_decent_number(left, right)
+            return [] if left < 0 || right < 0
+
+            if divisible_by_5?(left) && divisible_by_3?(right)
+              Array.new(right, BIGGER_DECENT_VALUE)
+                .concat(Array.new(left, SMALLER_DECENT_VALUE))
+            else
+              find_decent_number(left - 1, right + 1)
+            end
+          end
         end
-
-        def decent_digit_splitited
-          first_value = Sherlock.new(splitted_factor).decent_number
-          second_value = Sherlock.new(number_of_digits - splitted_factor).decent_number
-
-          first_value << second_value
-        end
-
-        def splitted_factor
-          (number_of_digits % BIGGER_DECENT_VALUE + number_of_digits)/2
-        end
-
-        attr_reader :number_of_digits
       end
     end
   end
 end
+
+# test_cases = gets.to_i
+# test_cases.times do
+#   input = gets.to_i
+#   sherlock = Hackerhank::Algorithms::Implementation::Sherlock.new
+#   puts sherlock.decent_number(input)
+# end
--- ruby/spec/algorithms/implementation/sherlock_and_the_beast_spec.rb
+++ ruby/spec/algorithms/implementation/sherlock_and_the_beast_spec.rb
@@ -1,8 +1,8 @@
 describe Hackerhank::Algorithms::Implementation::Sherlock do
-  let(:sherlock) { described_class.new(number_of_digits) }
+  subject(:sherlock) { described_class.new }
 
   describe '#decent_number' do
-    subject(:decent_number) { sherlock.decent_number }
+    subject(:decent_number) { sherlock.decent_number(number_of_digits) }
 
     context 'when number of digits is 3 digits' do
       let(:number_of_digits) { 3 }
@@ -30,7 +30,7 @@ describe Hackerhank::Algorithms::Implementation::Sherlock do
       context 'when number of digits are 13' do
         let(:number_of_digits) { 13 }
 
-        xit { expect(decent_number).to eq '55555533333' }
+        it { expect(decent_number).to eq '5553333333333' }
       end
     end
   end
