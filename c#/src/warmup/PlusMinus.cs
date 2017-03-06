3f15150743ec99cec6da249058f642d22e573d62
Alabê Duarte <alabeduarte@gmail.com>
2017-03-06 16:08:13 +1100

    Add "Plus Minus" solution using C#
    https://www.hackerrank.com/challenges/plus-minus

---
+++ c#/src/warmup/PlusMinus.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+class Solution {
+
+  private delegate bool Condition(int number);
+
+  private static double GetFractionOf(Condition condition, int[] numbers)
+  {
+    return (double) numbers.Where(n => condition(n)).Count()/numbers.Length;
+  }
+
+  static void Main(String[] args) {
+    // skip the first line
+    Console.ReadLine();
+
+    int[] numbers = Console.ReadLine()
+      .Split(' ')
+      .Select(n => Convert.ToInt32(n))
+      .ToArray();
+
+    Console.WriteLine(GetFractionOf(n => n > 0, numbers));
+    Console.WriteLine(GetFractionOf(n => n < 0, numbers));
+    Console.WriteLine(GetFractionOf(n => n == 0, numbers));
+  }
+}
