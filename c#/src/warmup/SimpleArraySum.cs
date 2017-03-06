9f35a850bcbb15f27273e77e096c774b84458647
Alabê Duarte <alabeduarte@gmail.com>
2017-03-06 13:22:05 +1100

    Add "Simple Array Sum" solution using C#
    https://www.hackerrank.com/challenges/simple-array-sum

---
+++ c#/src/warmup/SimpleArraySum.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+class Solution {
+
+  static void Main(String[] args) {
+    // skip the first line
+    Console.ReadLine();
+
+    int sum = Console.ReadLine()
+      .Split(' ')
+      .Select(n => Convert.ToInt32(n))
+      .Aggregate((currentNumber, nextNumber) => currentNumber + nextNumber);
+
+    Console.WriteLine(sum);
+  }
+}
