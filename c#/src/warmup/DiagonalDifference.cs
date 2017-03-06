1b30ffb2291009aa9ced30fb6f652b5b5b8ad2e0
Alabê Duarte <alabeduarte@gmail.com>
2017-03-06 15:42:25 +1100

    Add "Diagonal Difference" solution usinc C#
    https://www.hackerrank.com/challenges/diagonal-difference

---
+++ c#/src/warmup/DiagonalDifference.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+class Solution {
+
+  private static int[][] BuildMatrix(int matrixDimension)
+  {
+    return Enumerable.Range(0, matrixDimension)
+      .Select(_ => Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray())
+      .ToArray();
+  }
+
+  private static int SumAcrossThePrimaryDiagonal(int[][] matrix)
+  {
+    return Enumerable.Range(0, matrix.Length)
+      .Select(d => matrix[d][d])
+      .Aggregate((curr, next) => curr + next);
+  }
+
+  private static int SumAcrossTheSecondaryDiagonal(int[][] matrix)
+  {
+    return Enumerable.Range(0, matrix.Length)
+      .Select(d => matrix[d][(matrix.Length-1) - d])
+      .Aggregate((curr, next) => curr + next);
+  }
+
+  static void Main(String[] args) {
+    int matrixDimension = Convert.ToInt32(Console.ReadLine());
+    int[][] matrix = BuildMatrix(matrixDimension);
+
+    int diagonalDifference = Math.Abs(
+      SumAcrossThePrimaryDiagonal(matrix) - SumAcrossTheSecondaryDiagonal(matrix)
+    );
+
+    Console.WriteLine(diagonalDifference);
+  }
+}
