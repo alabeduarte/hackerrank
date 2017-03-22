2c385731376daccc073c772ff1ad483c2e14ff6e
Alabê Duarte <alabeduarte@gmail.com>
2017-03-22 14:47:59 +1100

    Remove unused function

--- php/algorithms/warmup/PlusMinus.php
+++ php/algorithms/warmup/PlusMinus.php
@@ -1,8 +1,4 @@
 <?php
-function filter($numbers, $criteria) {
-  return array_filter($numbers, $criteria);
-};
-
 $handle = fopen("php://stdin","r");
 
 $size = fgets($handle);
@@ -11,9 +7,9 @@ $numbers = array_map(
   explode(" ", fgets($handle))
 );
 
-$only_positive_numbers = filter($numbers, function($n) { return $n > 0; });
-$only_negative_numbers = filter($numbers, function($n) { return $n < 0; });
-$only_zeroes = filter($numbers, function($n) { return $n == 0; });
+$only_positive_numbers = array_filter($numbers, function($n) { return $n > 0; });
+$only_negative_numbers = array_filter($numbers, function($n) { return $n < 0; });
+$only_zeroes = array_filter($numbers, function($n) { return $n == 0; });
 
 echo count($only_positive_numbers)/$size, "\n";
 echo count($only_negative_numbers)/$size, "\n";
